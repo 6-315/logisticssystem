bf6a7a09725fca9e0fe1cbdab93e0096c7d89f0b
lmj1996 <38312369+lmj1996@users.noreply.github.com>
2018-06-07 10:01:19 +0800

    环境修建以及车辆部分代码的更改

--- logisticssystem/src/com/logistics/vehiclemanagement/service/impl/VehicleManagementServiceImpl.java
+++ logisticssystem/src/com/logistics/vehiclemanagement/service/impl/VehicleManagementServiceImpl.java
@@ -1,16 +1,32 @@
 package com.logistics.vehiclemanagement.service.impl;
 
+import com.logistics.domain.vehicle;
 import com.logistics.vehiclemanagement.dao.VehicleManagementDao;
+import com.logistics.vehiclemanagement.service.VehicleManagementService;
+
+import util.BuildUuid;
+import util.TimeUtil;
 /**
  * 车辆管理业务实现层
  * @author LW
  *
  */
-public class VehicleManagementServiceImpl {
+public class VehicleManagementServiceImpl implements VehicleManagementService{
 private VehicleManagementDao vehicleManagementDao;
 
 public void setVehicleManagementDao(VehicleManagementDao vehicleManagementDao) {
 	this.vehicleManagementDao = vehicleManagementDao;
+}
+
+@Override
+public int addVehicle(vehicle vehicleinfo) {
+	vehicleinfo.setVehicle_id(BuildUuid.getUuid());
+	vehicleinfo.setVehicle_acquisitiontime(TimeUtil.getStringSecond());
+	vehicleinfo.setVehicle_createtime(TimeUtil.getStringSecond());
+	vehicleinfo.setVehicle_modifytime(TimeUtil.getStringSecond());
+	vehicleinfo.setVehicle_mark("None");
+	vehicleManagementDao.saveOrUpdateObject(vehicleinfo);
+	return 1;
 }
 
 }
