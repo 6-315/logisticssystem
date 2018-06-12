2627733acf2f4b7a060f4d9ff940175168316c6f
LaiLiang315 <38312569+LaiLiang315@users.noreply.github.com>
2018-06-12 21:43:50 +0800

    修改查询

--- logisticssystem/src/com/logistics/transferstation/VO/UnitManagerVO.java
+++ logisticssystem/src/com/logistics/transferstation/VO/UnitManagerVO.java
@@ -18,16 +18,27 @@ public class UnitManagerVO {
 	 * 生成list集合
 	 */
 	private List<UnitManagerDTO> listUnitManagerDTO;
+	
+	private String position;
+	
+	
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
 	private int totalRecords = 0;
 	
 	private String unit_id;
 	
-	private String admin;
-	public String getAdmin() {
-		return admin;
+	private String unit_type;
+	
+	public String getUnit_type() {
+		return unit_type;
 	}
-	public void setAdmin(String admin) {
-		this.admin = admin;
+	public void setUnit_type(String unit_type) {
+		this.unit_type = unit_type;
 	}
 	public String getUnit_id() {
 		return unit_id;
@@ -76,10 +87,7 @@ public class UnitManagerVO {
 	 * 模糊查询
 	 */
 	private String search;
-	/**
-	 * 根据Id删除中转站信息
-	 */
-	private String idList;
+	
 	/**
 	 * 员工信息表的session
 	 */
@@ -90,12 +98,6 @@ public class UnitManagerVO {
 	}
 	public void setStaff_session(String staff_session) {
 		this.staff_session = staff_session;
-	}
-	public String getIdList() {
-		return idList;
-	}
-	public void setIdList(String idList) {
-		this.idList = idList;
 	}
 	public String getSearch() {
 		return search;
@@ -150,10 +152,10 @@ public class UnitManagerVO {
 	}
 	@Override
 	public String toString() {
-		return "UnitManagerVO [listUnitManagerDTO=" + listUnitManagerDTO + ", totalRecords=" + totalRecords
-				+ ", unit_id=" + unit_id + ", admin=" + admin + ", state=" + state + ", address=" + address + ", num="
-				+ num + ", superiorunit=" + superiorunit + ", pageIndex=" + pageIndex + ", pageSize=" + pageSize
-				+ ", totalPages=" + totalPages + ", search=" + search + ", idList=" + idList + ", staff_session="
+		return "UnitManagerVO [listUnitManagerDTO=" + listUnitManagerDTO + ", position=" + position + ", totalRecords="
+				+ totalRecords + ", unit_id=" + unit_id + ", unit_type=" + unit_type + ", state=" + state + ", address="
+				+ address + ", num=" + num + ", superiorunit=" + superiorunit + ", pageIndex=" + pageIndex
+				+ ", pageSize=" + pageSize + ", totalPages=" + totalPages + ", search=" + search + ", staff_session="
 				+ staff_session + ", havePrePage=" + havePrePage + ", haveNextPage=" + haveNextPage + "]";
 	}
 
