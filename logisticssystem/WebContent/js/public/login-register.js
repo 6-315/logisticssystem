ef764a7bca4a0871b2edb0aa82bf1d3298148c02
ystartor@gmail.com <ystartor@gmail.com>
2018-06-14 20:24:27 +0800

    合并代码

--- logisticssystem/WebContent/js/public/login-register.js
+++ logisticssystem/WebContent/js/public/login-register.js
@@ -3,22 +3,10 @@
     const userinfo_Vue = new Vue({
         el: '#login',
         data: {
-            name: '',
-            password: ''
+
         },
         methods: {
-            login: function () {
-                axios.post('/logisticssystem/loginregister/loginregister_login',{
-                    username:this.name,
-                    password:this.password
-                })
-                    .then(function(response){
 
-                    })
-                    .catch(function(error){
-                        console.log('失败:',error);
-                    });
-            }
         }
     })
 })()
