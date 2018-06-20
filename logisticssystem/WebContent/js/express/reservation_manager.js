e50ec1dff2b659525318d8ebd3c79cfc550ddac1
ystartor@gmail.com <ystartor@gmail.com>
2018-06-21 03:20:06 +0800

    提交数据

--- logisticssystem/WebContent/js/express/reservation_manager.js
+++ logisticssystem/WebContent/js/express/reservation_manager.js
@@ -11,13 +11,49 @@
             HaveNextPage: '',
             search: '',
             state: '',
-            unit: ''
-        }
+            unit: '',
+            isDistributed: ''
+        },
+        search: '',
+        page: '',
+        state: '',
+        unit: '',
+        isDistributed: ''
     }
     const view_reservation = new Vue({
         el: '#reservation_manager',
         data: reservationData,
-        methods: {},
+        methods: {
+            searchReservationNum: function () {
+                view_reservation.getAllData()
+            },
+            getAllData: function () {
+                $.ajax({
+                    url: '/logisticssystem/expressmanagement/expressmanagement_queryReservationInfo',
+                    type: 'POST',
+                    data: {
+                        'search': reservationData.search,
+                        'page': reservationData.page,
+                        'state': reservationData.state,
+                        'unit': reservationData.unit,
+                        'isDistributed': reservationData.isDistributed
+                    },
+                    success: function (data) {
+                        let reservationListVO = JSON.parse(data)
+                        reservationData.reservationVO.listReservationInfoDTO = reservationListVO.listReservationInfoDTO
+                        reservationData.reservationVO.pageIndex = reservationListVO.pageIndex
+                        reservationData.reservationVO.totalRecords = reservationListVO.totalRecords
+                        reservationData.reservationVO.pageSize = reservationListVO.pageSize
+                        reservationData.reservationVO.totalPages = reservationListVO.totalPages
+                        reservationData.reservationVO.HavePrePage = reservationListVO.HavePrePage
+                        reservationData.reservationVO.HaveNextPage = reservationListVO.HaveNextPage
+                        reservationData.reservationVO.search = reservationListVO.search
+                        reservationData.reservationVO.state = reservationListVO.state
+                        reservationData.reservationVO.unit = reservationListVO.unit
+                    }
+                })
+            }
+        },
         mounted() {
             //获取单位信息
             $.ajax({
