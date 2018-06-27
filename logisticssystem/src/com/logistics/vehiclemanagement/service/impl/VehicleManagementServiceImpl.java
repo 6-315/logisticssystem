21c0f7dfd4836cb4effe4834752cfdbd498598e7
lmj1996 <38312369+lmj1996@users.noreply.github.com>
2018-06-28 02:13:48 +0800

    1

--- logisticssystem/src/com/logistics/vehiclemanagement/service/impl/VehicleManagementServiceImpl.java
+++ logisticssystem/src/com/logistics/vehiclemanagement/service/impl/VehicleManagementServiceImpl.java
@@ -175,8 +175,8 @@ public class VehicleManagementServiceImpl implements VehicleManagementService {
 								}
 							}
 						} else {
-							vehicleCountHql = vehicleCountHql + " 1！=1 ";
-							listVehicleDTOCountHql = listVehicleDTOCountHql + " 1！=1 ";
+							vehicleCountHql = vehicleCountHql + " 1!=1 ";
+							listVehicleDTOCountHql = listVehicleDTOCountHql + " 1!=1 ";
 						}
 					} else if ("驾驶员".equals(postionInfo.getPosition_name())) {
 						if (staffInfo.getStaff_id() != null && staffInfo.getStaff_id().trim().length() > 0) {
@@ -189,8 +189,8 @@ public class VehicleManagementServiceImpl implements VehicleManagementService {
 									listVehicleDTOCountHql = listVehicleDTOCountHql + " vehicle_id='"
 											+ driverInfo.getDriver_vehicle() + "' ";
 								}else {
-									vehicleCountHql = vehicleCountHql + " 1！=1 ";
-									listVehicleDTOCountHql = listVehicleDTOCountHql + " 1！=1 ";
+									vehicleCountHql = vehicleCountHql + " 1!=1 ";
+									listVehicleDTOCountHql = listVehicleDTOCountHql + " 1!=1 ";
 								}
 							} 
 						}
